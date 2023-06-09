63fde18b19c411b224a647d9663fe2cd4a7e5e26
initialencounter <2911583893@qq.com>
2023-06-09 22:35:50 +0800

    fix: issue#1 TypeError

--- index.ts
+++ index.ts
@@ -6,12 +6,13 @@ import open_vits from 'koishi-plugin-open-vits'
 // 创建一个 Koishi 应用
 const ctx = new Context({
   port: 5140,
+  prefix: '.'
 })
 // 使用 OneBot 适配器的机器人
 ctx.plugin(onebot, {
     protocol: 'ws',
-    selfId: '3111720341',
-    endpoint: 'ws://127.0.0.1:32333',
+    selfId: '1114039391',
+    endpoint: 'ws://192.168.137.218:8081',
   })
 
 // 启用上述插件
@@ -20,13 +21,14 @@ ctx.plugin(sandbox)     // 提供调试沙盒
 // 启用glm-bot
 ctx.plugin(open_vits,{
     endpoint: 'https://api.vits.t4wefan.pub',
-    speaker_id: 3,
+    speaker_id: '3',
     max_length: 1024,
     waiting: true,
     waiting_text: '思考中...',
     recall: true,
     recall_time: 5000,
-    max_speakers: 3
+    max_speakers: 3,
+    translator: false
 })
 
 // 启动应用
